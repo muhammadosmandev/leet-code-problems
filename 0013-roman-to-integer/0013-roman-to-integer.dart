c1021ada28069fb77bf798a994af0061675b5454
Osman <muhammadosman.dev@gmail.com>
2026-08-06 20:20:06 +0500

    Time: 10 ms (67.69%), Space: 148.6 MB (90.77%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.dart
@@ -0,0 +1,27 @@
+class Solution {
+  int romanToInt(String s) {
+    Map<String, int> values = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000,
+    };
+
+    int result = 0;
+
+    for (int i = 0; i < s.length; i++) {
+      int current = values[s[i]]!;
+
+      if (i + 1 < s.length && current < values[s[i + 1]]!) {
+        result -= current;
+      } else {
+        result += current;
+      }
+    }
+
+    return result;
+  }
+}
