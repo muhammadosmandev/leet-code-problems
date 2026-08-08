a2a78f8be2275e9d18579f108e010f929c896046
Osman <muhammadosman.dev@gmail.com>
2026-08-08 20:44:26 +0500

    Time: 39 ms (92.52%), Space: 157.2 MB (93.46%) - LeetHub

---
+++ 0015-3sum/0015-3sum.dart
@@ -0,0 +1,43 @@
+class Solution {
+  List<List<int>> threeSum(List<int> nums) {
+    List<List<int>> result = [];
+
+    nums.sort();
+
+    for (int i = 0; i < nums.length - 2; i++) {
+      // Skip duplicate first values
+      if (i > 0 && nums[i] == nums[i - 1]) {
+        continue;
+      }
+
+      int left = i + 1;
+      int right = nums.length - 1;
+
+      while (left < right) {
+        int sum = nums[i] + nums[left] + nums[right];
+
+        if (sum == 0) {
+          result.add([nums[i], nums[left], nums[right]]);
+
+          // Skip duplicate values
+          while (left < right && nums[left] == nums[left + 1]) {
+            left++;
+          }
+
+          while (left < right && nums[right] == nums[right - 1]) {
+            right--;
+          }
+
+          left++;
+          right--;
+        } else if (sum < 0) {
+          left++;
+        } else {
+          right--;
+        }
+      }
+    }
+
+    return result;
+  }
+}
