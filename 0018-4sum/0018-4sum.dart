36217679b00754311c7fb165a87086c8611b75de
Osman <muhammadosman.dev@gmail.com>
2026-08-11 20:51:38 +0500

    Time: 20 ms (92.59%), Space: 157.9 MB (7.41%) - LeetHub

---
+++ 0018-4sum/0018-4sum.dart
@@ -0,0 +1,54 @@
+class Solution {
+  List<List<int>> fourSum(List<int> nums, int target) {
+    List<List<int>> result = [];
+
+    nums.sort();
+
+    int n = nums.length;
+
+    for (int i = 0; i < n - 3; i++) {
+      if (i > 0 && nums[i] == nums[i - 1]) {
+        continue;
+      }
+
+      for (int j = i + 1; j < n - 2; j++) {
+        if (j > i + 1 && nums[j] == nums[j - 1]) {
+          continue;
+        }
+
+        int left = j + 1;
+        int right = n - 1;
+
+        while (left < right) {
+          int sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+          if (sum == target) {
+            result.add([
+              nums[i],
+              nums[j],
+              nums[left],
+              nums[right],
+            ]);
+
+            while (left < right && nums[left] == nums[left + 1]) {
+              left++;
+            }
+
+            while (left < right && nums[right] == nums[right - 1]) {
+              right--;
+            }
+
+            left++;
+            right--;
+          } else if (sum < target) {
+            left++;
+          } else {
+            right--;
+          }
+        }
+      }
+    }
+
+    return result;
+  }
+}
